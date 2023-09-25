eae0783f985935a891ca614584418cb2c048ef3e
diazev20 <diazev20@gmail.com>
2023-09-25 05:29:14 -0500

    change route send mailer

--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { sendEmailController } = require('../controllers/emailController');
 
 // Define la ruta para enviar un correo electrónico y utiliza sendEmail como callback
-router.post('/send-email', sendEmailController);
+router.post('/sendEmail', sendEmailController);
 router.get('/', (req, res) => {
     res.send('This is my API mailer, welcome!');
 });
